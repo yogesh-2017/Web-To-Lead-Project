aef085541837306980c81cb328a7e5b98c19db2c
Yogesh Rathore <yogesh.rathore.0702@gmail.com>
2023-09-16 18:06:39 +0530

    Add reCAPTCHA validation

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-function beforeSubmit(){
-    let outputdate = document.querySelector(".outputdate");
-    let inputdate = document.querySelector(".inputdate");
-    console.log("inputDate.value", inputdate.value); //string --> date (en_US)
-    //conversion of date & satore it in salesforce org , convert the date as per your locale
-    //System.debug(UserInfo.getLocale()); to get locale
+let captchaChecked = false;
+function beforeSubmit(event){
+    if(captchaChecked){
+        let outputdate = document.querySelector(".outputdate");
+        let inputdate = document.querySelector(".inputdate");
+        console.log("inputDate.value", inputdate.value); //string --> date (en_US)
+        //conversion of date & satore it in salesforce org , convert the date as per your locale
+        //System.debug(UserInfo.getLocale()); to get locale
 
-    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
-    outputdate.value = formattedDate;
+        let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
+        outputdate.value = formattedDate;
+    }else{
+        alert("Please check the reCAPTCHA");
+        //prevent the submission form
+        event.preventDefault();
+    }
+    
 }
 function timestamp() { 
     var response = document.getElementById("g-recaptcha-response"); 
@@ -17,3 +25,9 @@ function timestamp() {
     } 
 } 
 setInterval(timestamp, 500);
+
+// As any user click on recatcha, this flag to be updated as true.
+//for this, use attribute : data-callback
+function captchaSuccess() {
+    captchaChecked = true;
+}
