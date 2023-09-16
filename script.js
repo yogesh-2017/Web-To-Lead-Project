6366316887425355e03dc6782302ee88ee11505c
Yogesh Rathore <yogesh.rathore.0702@gmail.com>
2023-09-16 17:47:47 +0530

    Add reCAPTCHA

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function beforeSubmit(){
     let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
     outputdate.value = formattedDate;
 }
+function timestamp() { 
+    var response = document.getElementById("g-recaptcha-response"); 
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+    } 
+} 
+setInterval(timestamp, 500);
